194d94281b1ea329ad1f1172da189dd77b684e4e
HG-0-1 <alfayezh844@gmail.com>
2025-09-21 17:33:33 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: helfayez <helfayez@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 15:25:30 by helfayez          #+#    #+#             */
-/*   Updated: 2025/09/15 16:54:59 by helfayez         ###   ########.fr       */
+/*   Updated: 2025/09/21 14:49:47 by helfayez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,12 @@ char	*return_line(char *line)
 		i++;
 	if (line[i] == '\n')
 		i++;
-	mallo = NULL;//malloc(i + 1);
+	mallo = malloc(i + 1);
 	if (!mallo)
+	{
+		free(mallo);
 		return (NULL);
+	}
 	while (j < i)
 	{
 		mallo[j] = line[j];
@@ -46,6 +49,7 @@ char	*c_save(size_t *i, char *line)
 		(*i)++;
 	if (!line[*i])
 	{
+		free(line);
 		return (NULL);
 	}
 	(*i)++;
@@ -79,30 +83,31 @@ char	*save_my_line(char *line)
 	return (col_line);
 }
 
-char	*read_line(int fd, char *line)
+char	*read_line(int fd, char *line, char *buffer)
 {
 	int		read_fa;
 	char	*new_line;
-	char	*buffer;
 
-	buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer)
-		if (ft_free(line))
-			return (NULL);
+	{
+		free(line);
+		return (NULL);
+	}
 	read_fa = 1;
 	while (!ft_strchr(line, '\n') && read_fa > 0)
 	{
 		read_fa = read(fd, buffer, BUFFER_SIZE);
 		if (read_fa < 0)
-			if (ft_free(buffer))
-				if (ft_free(line))
-					return (NULL);
+			if (ft_free(line))
+				return (NULL);
 		buffer[read_fa] = '\0';
-		new_line = ft_strjoin(line, buffer);
+		new_line = ft_strjoin(l , buffer);
+		if (!new_line)
+			if (ft_free(line))
+				return (NULL);
 		free(line);
 		line = new_line;
 	}
-	free(buffer);
 	return (line);
 }
 
@@ -110,16 +115,13 @@ char	*get_next_line(int fd)
 {
 	static char	*line;
 	char		*res;
+	char		*buffer;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
-	{
-		free(line);
-		line = NULL;
 		return (NULL);
-	}
-	if (!line)
-		line = ft_strdup("");
-	line = read_line(fd, line);
+	buffer = malloc(BUFFER_SIZE + 1);
+	line = read_line(fd, line, buffer);
+	free(buffer);
 	if (!line)
 		return (NULL);
 	if (line[0] == '\0')
@@ -129,8 +131,41 @@ char	*get_next_line(int fd)
 		return (NULL);
 	}
 	res = return_line(line);
-	if (!res)
-		return (NULL);
 	line = save_my_line(line);
+	if (!res)
+		if (ft_free(line))
+			if (ft_free(res))
+				return (NULL);
 	return (res);
 }
+
+// #include <fcntl.h>     
+// #include <unistd.h>    
+// #include <stdio.h>      
+// #include <stdlib.h>    
+
+// #include "get_next_line.h" 
+
+// int main(void)
+// {
+//     int     fd;
+//     char    *line;
+
+//    
+//      fd = open("h.txt", O_RDONLY);
+//     if (fd < 0)
+//     {
+//         perror("open");
+//         return (1);
+//     }
+
+//   
+//     while ((line = get_next_line(fd)) != NULL)
+//     {
+//         printf("%s", line);
+//         free(line);
+//     }
+
+//     close(fd);
+
+// }
